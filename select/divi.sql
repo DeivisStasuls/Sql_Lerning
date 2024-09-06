d2cd196680fcc10afbdedd63d478042ae19b4c33
deivis <you@example.com>
2024-09-06 10:41:03 +0300

    add select with limit

--- select/divi.sql
+++ select/divi.sql
@@ -2,3 +2,4 @@ use sql_store;
 -- select first_name,last_name,points,points*4.1 as "points markiplied"  from customers;
 select * from customers;
 select distinct * from customers;
+select * from customers limit 5;
