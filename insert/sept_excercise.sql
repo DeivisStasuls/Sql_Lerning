80b024615f65613a16f7f109f7f12f4e1c64b2e3
deivis <you@example.com>
2024-10-03 09:18:01 +0300

    db design class join/delete excercises

---
+++ insert/sept_excercise.sql
@@ -0,0 +1,4 @@
+use sql_store;
+select * from order_items;
+
+select * from customers where customer_id in (10,11,12);
---
+++ insert/sesi_excercise.sql
@@ -0,0 +1,8 @@
+-- 1. uzdevums
+use sql_store;
+insert into customers(first_name,
+    last_name,
+    address,
+    city,
+    state)values("Deivis","Stasuls","berzu iela","freak city","FC");
+select * from customers;
